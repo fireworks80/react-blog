26f8d7a27a0e7ff3672444de5dbf7860fe730b28
fireworks <seungchulhan@gmail.com>
2023-10-23 23:54:06 +0900

    feat: user 스키마 /모델 생성

---
+++ backend/src/models/user.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// instance methods
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  // instance를 가리킨다
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  return bcrypt.compare(password, this.hashedPassword);
+};
+
+// static methods
+UserSchema.statics.findByUsername = function (username) {
+  // this는 model을 가리킨다
+  return this.findOne({ username });
+};
+
+const User = mongoose.model('User', UserSchema);
+
+export { User };
