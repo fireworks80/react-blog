976a8c6cdcd8e20eb6de935ac79600d739012868
fireworks <seungchulhan@gmail.com>
2023-10-27 22:58:08 +0900

    feat: 토큰 재발급 추가

--- backend/src/lib/jwtMiddleware.js
+++ backend/src/lib/jwtMiddleware.js
@@ -17,15 +17,16 @@ const jwtMiddleware = async (ctx, next) => {
       decoded,
     );
 
-    // const now = Math.floor(Date.now() / 1000);
-    // if (decoded.exp - now < 60 * 60 * 24 * 7) {
-    //   const user = await User.findById(decoded._id);
-    //   const token = user.generateToken();
-    //   ctx.cookies.set(process.env.COOKIE_NAME, token, {
-    //     maxAge: 1000 * 60 * 60 * 24 * 7,
-    //     httpOnly: true,
-    //   });
-    // }
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      ctx.cookies.set(process.env.COOKIE_NAME, token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+      });
+    }
+
     return next();
   } catch (e) {
     return next();
